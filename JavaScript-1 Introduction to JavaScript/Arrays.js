5f106ae597891509d8d3489bcfea08caedb5b0e2
Sourabh <bonamrithwik@gmail.com>
2025-07-01 18:03:02 +0530

    nothing big changed

--- JavaScript-1 Introduction to JavaScript/Arrays.js
+++ JavaScript-1 Introduction to JavaScript/Arrays.js
@@ -6,48 +6,49 @@ let data = [1 ,'scaler' ,true ,null ,undefined] // indexed
 console.log(data)
 
 
-// length of the array
+// // length of the array
 
 console.log(data.length)
 
-// methods and properties of an array
+// // methods and properties of an array
 
-// 1. How to access array elements
+// // 1. How to access array elements
 
  console.log(data[3])
 
- // methods to do operations an array 
+//  // methods to do operations an array 
 
  let cars = ['BMW' , 'Toyota' , 'Urus']
 
 
- // push method
+//  // push method
  cars.push('Audi')
  console.log(cars)
-//  push method allows you to add a value at the end of an array
+// //  push method allows you to add a value at the end of an array
 
 
-// pop method
+// // pop method
 cars.pop()
 cars.pop()
-//  pop method allows you to delete a value at the end of an array
+console.log(cars)
+// //  pop method allows you to delete a value at the end of an array
 
 
-// shift and unshift
+// // shift and unshift
 
 cars.unshift('Mercedes')
 console.log(cars)
 
-// shift method
+// // shift method
 
 cars.shift()
 console.log(cars)
 
 
-// split and join - this we can use to add or deleet something from between
+// // split and join - this we can use to add or deleet something from between
 
 
-// splice
+// // splice
 
 let arr1 = [20 , 30 , 40 , 50 , 60 , 70]
 arr1.push(400)
