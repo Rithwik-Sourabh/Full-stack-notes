4f34ae89935796aa0b656e76ced79381a931be52
Sourabh <bonamrithwik@gmail.com>
2025-07-07 11:24:59 +0530

    nothing

--- JavaScript-3 Lexical Scope and Closures/Lexical.js
+++ JavaScript-3 Lexical Scope and Closures/Lexical.js
@@ -1,16 +1,16 @@
 function parent() {
-  var a = 20;
-  console.log(a);
+    var a = 20;
+    console.log(a);
 
-  function child() {
-    function child2() {
-      console.log(a);
+    function child() {
+        function child2() {
+            console.log(a);
+        }
+        a = 30;
+        child2();
+
     }
-    a = 30;
-    child2();
- 
-  }
-  child();
+    child();
 }
 
 parent();
